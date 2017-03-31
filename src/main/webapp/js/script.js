a491a20d10a10887fed36f6e477c284b3d0b4106
Abdelcadir Karim <AbdelcadirKa@valueteam.com>
2017-03-31 10:32:27 +0200

    fix files position

---
+++ src/main/webapp/js/script.js
@@ -0,0 +1,12 @@
+// document.addEventListener("DOMContentLoaded",
+//   function (event) {
+//
+//     // Unobtrusive event binding
+//     document.querySelector("#q1")
+//       .addEventListener("click",
+//   }
+// );
+
+function click(){
+  document.querySelector("#q1").innerHTML="<img src='images/x-tris.jpg'></img>";
+}
