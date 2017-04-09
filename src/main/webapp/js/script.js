c8fea6347f42d8d9027f9e86ea43ccaa31631686
PCHAKIM\Hakim <hakim98bologna@gmail.com>
2017-04-09 18:41:30 +0200

    no message

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,27 +1,26 @@
 
-
-// function click(){
-//   document.querySelector("#q1").innerHTML="<img src='images/x-tris.jpg'></img>";
-// }
-document.addEventListener("DOMContentLoaded",
-  function (event) {
-
-    // Unobtrusive event binding
-    document.querySelector("button")
-      .addEventListener("click", function () {
-
-
-        // Call server to get the name
-        $ajaxUtils
-          .sendGetRequest("rest/game",
-            function (request) {
-              var arrayGriglia = request.responseText;
-
-                          document.querySelector("")
-                            .innerHTML = "";
-
-            });
-
-      });
-  }
-);
+document.getElementById("q1").onclick = function (){
+  document.querySelector("#q1").innerHTML="<img src='images/x-tris.jpg'></img>";
+};
+// document.addEventListener("DOMContentLoaded",
+//   function (event) {
+//
+//     // Unobtrusive event binding
+//     document.querySelector("button")
+      // .addEventListener("click", function () {
+      //
+      //
+      //   // Call server to get the name
+      //   $ajaxUtils
+      //     .sendGetRequest("rest/game",
+      //       function (request) {
+      //         var arrayGriglia = request.responseText;
+      //
+      //                     document.querySelector("")
+      //                       .innerHTML = "";
+      //
+      //       });
+      //
+      // });
+//   }
+// );
